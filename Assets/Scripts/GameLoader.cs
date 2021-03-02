4952957f91776110dfec7b292874c0ca17c27d4b
Tekknobot <thrillatron@hotmail.com>
2021-03-01 23:25:43 -0500

    Add Match 4 game.

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour
+{
+    public Button matchButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        matchButton.GetComponent<Button>().onClick.AddListener(StartMatchOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartMatchOnClick() {
+        SceneManager.LoadScene("game", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/KontrolLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KontrolLoader : MonoBehaviour
+{
+    public Button kontrolButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kontrolButton.GetComponent<Button>().onClick.AddListener(StartKontrolOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartKontrolOnClick() {
+        SceneManager.LoadScene("default", LoadSceneMode.Single);
+    }
+}
