f8f35f830f9ebf0af14c33a87b54c789efd0456d
Tekknobot <thrillatron@hotmail.com>
2021-03-11 20:25:57 -0500

    Change Camera background on Operator header click.

--- Assets/Scripts/CameraBackground.cs
+++ Assets/Scripts/CameraBackground.cs
@@ -1,21 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
  
 public class CameraBackground : MonoBehaviour
 {
-    public Color red = Color.red;
+    public Color defaultcolor = Color.red;
+    public Color alternatecolor = Color.blue;
+    public Color ppn = Color.grey;
+
     Camera cm;
+    bool alternate = true;
+
+    public Button previous;
+    public Button play;
+    public Button next;
+
+    public Button OperatorHeader;
  
     void Start()
     {
         cm = GetComponent<Camera>();
+    
+        previous.GetComponent<Image>().color = defaultcolor;
+        play.GetComponent<Image>().color = defaultcolor;
+        next.GetComponent<Image>().color = defaultcolor;  
+
+        OperatorHeader.GetComponent<Button>().onClick.AddListener(OperatorHeaderClick);      
     }
  
-    void Update()
+    void OperatorHeaderClick()
     {
-        if (Input.GetKeyUp ("Jump")) 
-        {
-            cm.backgroundColor = red;
+        alternate = !alternate;
+        if (alternate) {
+            cm.backgroundColor = defaultcolor;
+            previous.GetComponent<Image>().color = defaultcolor;
+            play.GetComponent<Image>().color = defaultcolor;
+            next.GetComponent<Image>().color = defaultcolor;
+        }
+        else {
+            cm.backgroundColor = alternatecolor;
+            previous.GetComponent<Image>().color = ppn;
+            play.GetComponent<Image>().color = ppn;
+            next.GetComponent<Image>().color = ppn;                
         }
     }
 }
